53533e36658eeee6a53ae7ade3ec99e8e48c8bc6
Mark Gladkikh <17348049+sockcrates@users.noreply.github.com>
2022-01-30 14:53:34 -0500

    Pass specs

--- src/List/List.test.tsx
+++ src/List/List.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import List from './List';
 
 describe('<List>', () => {
   test('Empty list', () => {
---
+++ src/List/List.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function List(): JSX.Element {
+  return (
+    <div>
+      You&apos;re all done 😃
+    </div>
+  );
+}
+
+export default List;
