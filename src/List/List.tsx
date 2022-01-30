0e11217f76b0c9b6f1d7d41b874f8c4c00b8c005
Mark Gladkikh <17348049+sockcrates@users.noreply.github.com>
2022-01-30 17:09:09 -0500

    Animate list

--- src/List/List.tsx
+++ src/List/List.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+import styles from './List.module.scss';
 
 interface Item {
   id: string;
@@ -8,6 +11,8 @@ interface Item {
 
 function List(): JSX.Element {
   const [items, setItems] = useState<Array<Item>>([]);
+
+  const duration = 300;
 
   const handleAddItem = () => {
     const id = `${new Date().toUTCString()}${Math.random()}`;
@@ -20,25 +25,38 @@ function List(): JSX.Element {
   };
 
   return (
-    <div>
+    <div className={styles.list}>
       <button onClick={handleAddItem} type="button">Add Item</button>
 
       <button onClick={handleRemoveItem} type="button">Remove Item</button>
 
       {items.length ? (
-        items.map((item) => (
-          <div key={item.id}>
-            <label htmlFor="name">
-              Item name:
-              <input id="name" placeholder="Item name" type="text" />
-            </label>
+        <TransitionGroup>
+          {items.map((item) => (
+            <CSSTransition
+              classNames={{
+                enter: styles.listEnter,
+                enterActive: styles.listEnterActive,
+                exit: styles.listExit,
+                exitActive: styles.listExitActive,
+              }}
+              style={{ '--duration': `${duration}ms` }}
+              timeout={duration}
+            >
+              <div key={item.id}>
+                <label htmlFor="name">
+                  Item name:
+                  <input id="name" placeholder="Item name" type="text" />
+                </label>
 
-            <label htmlFor="price">
-              Price:
-              <input id="price" placeholder="Price (optional)" type="text" />
-            </label>
-          </div>
-        ))
+                <label htmlFor="price">
+                  Price:
+                  <input id="price" placeholder="Price (optional)" type="text" />
+                </label>
+              </div>
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
       ) : ('You\'re all done 😃')}
     </div>
   );
