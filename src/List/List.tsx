143b5b8364b17473fd354e73aea8dd8d3a61dca3
Mark Gladkikh <17348049+sockcrates@users.noreply.github.com>
2022-01-30 15:24:51 -0500

    Implement adding items

--- src/List/List.tsx
+++ src/List/List.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+interface Item {
+  id: string;
+  name: string;
+  price?: number;
+}
 
 function List(): JSX.Element {
+  const [items, setItems] = useState<Array<Item>>([]);
+
+  const handleClick = () => {
+    const id = new Date().toUTCString();
+    setItems([...items, { id, name: '' }]);
+  };
+
   return (
     <div>
-      You&apos;re all done 😃
+      <button onClick={handleClick} type="button">Add Item</button>
+
+      {items.length ? (
+        items.map((item) => (
+          <div key={item.id}>
+            <label htmlFor="name">
+              Item name:
+              <input id="name" placeholder="Item name" type="text" />
+            </label>
+
+            <label htmlFor="price">
+              Price:
+              <input id="price" placeholder="Price (optional)" type="text" />
+            </label>
+          </div>
+        ))
+      ) : ('You\'re all done 😃')}
     </div>
   );
 }
